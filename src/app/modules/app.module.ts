14524c370ff635a277637177af028626564049cb
Caleb Gosiak <G554146@GENMILLS.COM>
2017-03-06 08:18:19 -0600

    Polishing

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,15 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from '../components/app.component';
+import { BrowserModule }                    from '@angular/platform-browser';
+import { ROUTES }                           from '../routing/routes';
+import { RouterModule }                     from "@angular/router";
+import { NgModule }                         from '@angular/core';
+import { FormsModule }                      from '@angular/forms';
+import { HttpModule, JsonpModule }          from '@angular/http';
+import { AppComponent }                     from '../components/app.component';
+import { ErrorComponent }                   from '../components/error.component';
+import { HomeComponent }                    from '../components/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(ROUTES),
     BrowserModule,
     FormsModule,
     HttpModule
